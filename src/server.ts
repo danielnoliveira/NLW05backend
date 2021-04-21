381c8ecc605af7493167484c0a97723824af7e5b
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-04-20 22:17:22 -0300

    Second day

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,18 @@
 import express from 'express';
+import {routes} from './routes';
+import './database';
 
 const app = express();
+
 
 // GET = buscar
 // POST = criação
 // PUT = alteração
 // DELETE = deletar
 // PATCH = alterar uma informação especifica
+app.use(express.json());
 
-app.get('/',(req,res) => {
-  return res.json({msg: 'Ola NLW 05'});
-});
-
-app.post('/users',(req,res)=>{
-  return res.json({msg:'Usuario salvo com sucesso!!!'});
-});
-
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server run on port ${PORT}`));
